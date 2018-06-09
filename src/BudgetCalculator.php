4ebe8d8dcf8c53ab754e354d162a1fc555e6854f
Ken Lin <kenlin@kkbox.com>
2018-06-09 16:08:09 +0800

    Valid dates and 0

--- src/BudgetCalculator.php
+++ src/BudgetCalculator.php
@@ -8,7 +8,26 @@ class BudgetCalculator
      */
     public function __construct($startDate, $endDate)
     {
+        if ( ! $this->isValidDatePeriod($startDate, $endDate)) {
+            throw new Exception('Invalid date');
+        }
+
         $this->startDate = $startDate;
         $this->endDate = $endDate;
+    }
+
+    public function calculate()
+    {
+
+
+        return 0;
+    }
+
+    private function isValidDatePeriod($start, $end)
+    {
+        $startDate = new DateTime($start);
+        $endDate = new DateTime($end);
+
+        return $endDate >= $startDate;
     }
 }
--- tests/BudgetCalculatorTest.php
+++ tests/BudgetCalculatorTest.php
@@ -2,15 +2,56 @@
 
 require __DIR__ . '/../src/BudgetCalculator.php';
 
-class BudgetCalculatorTest extends \PHPUnit\Framework\TestCase
+use PHPUnit\Framework\TestCase;
+
+class BudgetCalculatorTest extends TestCase
 {
     /**
-      * @test
-      */
+     * @test
+     */
     public function it_should_instaniate_class()
     {
         $actual = new BudgetCalculator('2018/01/01', '2018/01/31');
 
         $this->assertInstanceOf(BudgetCalculator::class, $actual);
     }
+
+    /**
+     * @test
+     */
+    public function it_should_throw_exception_when_end_bigger_than_start()
+    {
+        // Arrange
+        $this->expectException(\Exception::class);
+
+        // Act
+        $start = '2018/02/03';
+        $end =   '2018/02/01';
+
+        new BudgetCalculator($start, $end);
+    }
+
+    /**
+     * @test
+     * @dataProvider datesProvider
+     */
+    public function getBudget($start, $end, $expected)
+    {
+        // Arrange
+        $calculator = new BudgetCalculator($start, $end);
+
+        // Act
+        $actual = $calculator->calculate();
+
+        // Assert
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function datesProvider()
+    {
+        return [
+            ['2018/03/01', '2018/03/31', 0],
+        ];
+    }
+
 }
