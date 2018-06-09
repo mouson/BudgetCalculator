320589a1cbbb5ddc7ccba2fb2232930529b7764d
mouson chen <mouson@gmail.com>
2018-06-10 00:02:47 +0800

    import data model

--- src/BudgetCalculator.php
+++ src/BudgetCalculator.php
@@ -1,10 +1,21 @@
 <?php
 namespace Mouson\BudgetCalculator;
 
+use BudgetModel;
 use Carbon\Carbon;
 
 class BudgetCalculator
 {
+    /**
+     * @var BudgetModel
+     */
+    private $model;
+
+    public function __construct(BudgetModel $model)
+    {
+
+        $this->model = $model;
+    }
 
     public function calculate(Carbon $start, Carbon $end)
     {
