157dff2c94919192cb9f19566e57e7079dff5f82
mouson chen <mouson@gmail.com>
2018-06-09 23:37:18 +0800

    exception handle pass

--- src/BudgetCalculator.php
+++ src/BudgetCalculator.php
@@ -1,6 +1,15 @@
 <?php
 namespace Mouson\BudgetCalculator;
 
+use Carbon\Carbon;
+
 class BudgetCalculator
 {
+
+    public function calculate(Carbon $start, Carbon $end)
+    {
+        if ($end->lt($start)) {
+            throw new \InvalidArgumentException('Argument Invalid!!');
+        }
+    }
 }
--- tests/BudgetCalculatorTest.php
+++ tests/BudgetCalculatorTest.php
@@ -1,8 +1,26 @@
 <?php
 namespace Tests\BudgetCalculatorTest;
 
+use Carbon\Carbon;
+use Mouson\BudgetCalculator\BudgetCalculator;
 use PHPUnit\Framework\TestCase;
 
 class BudgetCalculatorTest extends TestCase
 {
+
+    /**
+     * @test
+     */
+    public function testDateInvalidate()
+    {
+        /** Arrange */
+        $target = new BudgetCalculator();
+
+        /** Assume */
+        $this->expectException(\InvalidArgumentException::class);
+
+        /** Act */
+        $actual = $target->calculate(new Carbon('2018-05-05'), new Carbon('2018-01-01'));
+
+    }
 }
